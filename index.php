aa8ec7cd23ffb34ca59d75dce723a665b928de9c
ehcube <79380692+ehcube@users.noreply.github.com>
2021-03-16 14:28:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<?php
+include(“includes/config.php”);
+
+if(isset($_SESSION[‘userLoggedin’])) {
+    $userLoggedIn=$_SESSION[‘userLoggedIn’];
+}
+ else {
+       header(“Location: register.php”);
+}
+  
+?>
+
 <html>
 <head>
 	<title>Welcome to Slotify!</title>
