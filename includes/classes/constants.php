308994988c73ed1a68abfbc686d1fc0ec2f899b5
ehcube <79380692+ehcube@users.noreply.github.com>
2021-03-16 14:24:19 +0530

    Update constants.php

--- includes/classes/constants.php
+++ includes/classes/constants.php
@@ -10,7 +10,8 @@ class Constants {
 	public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
 	public static $firstNameCharacters = "Your first name must be between 5 and 25 characters";
 	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
-	
+	public static $usernameTaken = “This username exists already” ; 
+
     
 }
 ?>
